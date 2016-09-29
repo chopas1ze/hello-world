652451665efe4fb2a5be1309050b8d3d3e679456
Chopas1zE <chopas1ze@gmail.com>
2016-09-29 22:29:44 +0900

    9월 29일 집 commit

--- webdemo/src/guestdemo/action/ViewAction.java
+++ webdemo/src/guestdemo/action/ViewAction.java
@@ -8,12 +8,15 @@ import guestdemo.dto.BoardDTO;
 
 public class ViewAction {
 	public void execute(HttpServletRequest req, HttpServletResponse resp){
+		//list.jsp에서 글번호(num)값을 받는다.
 		String number = req.getParameter("num");
 		BoardDAO dao = BoardDAO.getInstance();
+		//readCountMethod를 통해 조회수값을 먼저 증가시킨다.
 		dao.readCountMethod(Integer.parseInt(number));
+		//viewMethod를 통해 dto객체에 num,writer,readcount,subject,email,content,upload,ref,re_step,re_level 값을 받는다.
 		BoardDTO dto = dao.viewMethod(Integer.parseInt(number));
+		//request영역에 dto객체를 저장한다
 		req.setAttribute("dto", dto);
-		
 		
 	}//end execute();
 	
